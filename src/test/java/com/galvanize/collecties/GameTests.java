4befe6a6a459fa56d4dea7abfdf18fad70eb9d5d
Connor Steiling <connor.steiling.xaa6@statefarm.com>
2023-06-22 10:07:05 -0500

    All tests passing and coverage at 90%

--- src/test/java/com/galvanize/collecties/GameTests.java
+++ src/test/java/com/galvanize/collecties/GameTests.java
@@ -119,43 +119,44 @@ public class GameTests {
         Assertions.assertEquals("Would you like to capture the wild Rextore?", output.split("\\r?\\n")[53]);
 
     }
+// The message outputted by missing was taken out when the improved combat feature was added.
+//    @Test
+//    public void bothMiss() {
+//        // 11 - VOLCANIC is picked as BIOME
+//        // 10001 - Random number given to first Collectie
+//        // 1 - Exploring goes to encounter logic
+//        // 0 - Finds first Collectie in this BIOME
+//        // 10002 - Random number given to cloned Collectie
+//        // 51 - our Rextore misses
+//        // 51 - other Rextore misses
+//        hookIntoRandom(11, 10001, 1, 0, 10002, 51, 51);
+//        disableGameSleep();
+//        String output = runGameWithInput("n", "2", "1", "5");
+//        enableGameSleep();
+//        displayOutputLines(output);
+//        Assertions.assertTrue(output.contains("Rextore 10001 has fallen and is unconscious!"));
+//        Assertions.assertEquals("Rextore 10001 survives the blow!", output.split("\\r?\\n")[56]);
+//    }
 
-    @Test
-    public void bothMiss() {
-        // 11 - VOLCANIC is picked as BIOME
-        // 10001 - Random number given to first Collectie
-        // 1 - Exploring goes to encounter logic
-        // 0 - Finds first Collectie in this BIOME
-        // 10002 - Random number given to cloned Collectie
-        // 51 - our Rextore misses
-        // 51 - other Rextore misses
-        hookIntoRandom(11, 10001, 1, 0, 10002, 51, 51);
-        disableGameSleep();
-        String output = runGameWithInput("n", "2", "1", "5");
-        enableGameSleep();
-        displayOutputLines(output);
-        Assertions.assertEquals("The wild Rextore manages to survive the blow!", output.split("\\r?\\n")[50]);
-        Assertions.assertEquals("Rextore 10001 survives the blow!", output.split("\\r?\\n")[53]);
-    }
-
-    @Test
-    public void fightWithMissThenGetHit() {
-        // 11 - VOLCANIC is picked as BIOME
-        // 10001 - Random number given to first Collectie
-        // 1 - Exploring goes to encounter logic
-        // 0 - Finds first Collectie in this BIOME
-        // 10002 - Random number given to cloned Collectie
-        // 51 - our Rextore misses
-        // 1 - other Rextore hits
-        hookIntoRandom(11, 10001, 1, 0, 10002, 51, 1);
-        disableGameSleep();
-        String output = runGameWithInput("n", "2", "1","3","1","6","5");
-        enableGameSleep();
-        displayOutputLines(output);
-        Assertions.assertEquals("The wild Rextore manages to survive the blow!", output.split("\\r?\\n")[50]);
-        Assertions.assertEquals("Rextore 10001 has fallen and is unconscious!", output.split("\\r?\\n")[53]);
-        Assertions.assertEquals("[1] Rextore 10001 (Rextore) [UNCONSCIOUS]", output.split("\\r?\\n")[69]);
-    }
+    // The message outputted by missing was taken out when the improved combat feature was added.
+    //    @Test
+//    public void fightWithMissThenGetHit() {
+//        // 11 - VOLCANIC is picked as BIOME
+//        // 10001 - Random number given to first Collectie
+//        // 1 - Exploring goes to encounter logic
+//        // 0 - Finds first Collectie in this BIOME
+//        // 10002 - Random number given to cloned Collectie
+//        // 51 - our Rextore misses
+//        // 1 - other Rextore hits
+//        hookIntoRandom(11, 10001, 1, 0, 10002, 51, 1);
+//        disableGameSleep();
+//        String output = runGameWithInput("n", "2", "1","3","1","6","5");
+//        enableGameSleep();
+//        displayOutputLines(output);
+//        Assertions.assertEquals("The wild Rextore manages to survive the blow!", output.split("\\r?\\n")[50]);
+//        Assertions.assertEquals("Rextore 10001 has fallen and is unconscious!", output.split("\\r?\\n")[53]);
+//        Assertions.assertEquals("[1] Rextore 10001 (Rextore) [UNCONSCIOUS]", output.split("\\r?\\n")[69]);
+//    }
     @Test
     public void fightWhileProtectorUnconsciousRuns(){
         // 11 - VOLCANIC is picked as BIOME
@@ -168,12 +169,12 @@ public class GameTests {
         // 1 - Exploring goes to encounter logic
         // 0 - Finds first Collectie in this BIOME
         // 10003 - Random number given to cloned Collectie
-        hookIntoRandom(11, 10001, 1, 0, 10002, 51, 1, 1, 0, 10003);
+        hookIntoRandom(11, 10001, 1, 0, 10002, 51, 1, 51, 1, 51, 1, 51, 1, 51, 1, 51, 1, 51, 1, 51, 1, 51, 1, 51, 1, 1, 0, 10003);
         disableGameSleep();
         String output = runGameWithInput("n", "2", "1","2","1","5");
         enableGameSleep();
         displayOutputLines(output);
-        Assertions.assertEquals("Your protector is unconscious so you GTFO!!", output.split("\\r?\\n")[95]);
+        Assertions.assertEquals("Your protector is unconscious so you GTFO!!", output.split("\\r?\\n")[93]);
     }
     @Test
     public void usingRedDodudRevivesCollectie(){
@@ -186,13 +187,13 @@ public class GameTests {
         // 1 - other Rextore hits
         // 51 - Exploring finds item
         // 0 - Finds RedDodud
-        hookIntoRandom(11, 10001, 1, 0, 10002, 51, 1, 51, 0);
+        hookIntoRandom(11, 10001, 1, 0, 10002, 51, 1, 51, 1, 51, 1, 51, 1, 51, 1, 51, 1, 51, 1, 51, 1, 51, 1, 51, 1, 51, 0);
         disableGameSleep();
         String output = runGameWithInput("n", "2", "1","2","3","3","1","1","6","5");
         enableGameSleep();
         displayOutputLines(output);
-        Assertions.assertEquals("You gave Rextore 10001 the RedDodud Sugar Free.", output.split("\\r?\\n")[84]);
-        Assertions.assertEquals("Rextore 10001 has been revived!", output.split("\\r?\\n")[85]);
+        Assertions.assertEquals("?: You give Rextore 10001 the RedDodud Sugar Free", output.split("\\r?\\n")[81]);
+        Assertions.assertEquals("Rextore 10001 has been revived!", output.split("\\r?\\n")[83]);
     }
     @Test
     public void usingRedDodudOnConsciousCollectieGivesMessage(){
@@ -207,11 +208,11 @@ public class GameTests {
         // 0 - Finds RedDodud   - only 2 items in VOLCANIC
         hookIntoRandom(11, 10001, 51, 0);
         disableGameSleep();
-        String output = runGameWithInput("n", "2","3","3","1","1","6","5");
+        String output = runGameWithInput("n","2","3","3","1","1","6","5");
         enableGameSleep();
         displayOutputLines(output);
-        System.out.println(output.split("\\r?\\n")[84]);
-        Assertions.assertEquals("You gave Rextore 10001 the RedDodud Sugar Free.", output.split("\\r?\\n")[84]);
-        Assertions.assertEquals("However, Rextore 10001 was already conscious, so nothing happened!", output.split("\\r?\\n")[85]);
+        System.out.println(output);
+        Assertions.assertEquals("You gave Rextore 10001 the RedDodud Sugar Free.", output.split("\\r?\\n")[38]);
+        Assertions.assertEquals("However, Rextore 10001 was already conscious, so nothing happened!", output.split("\\r?\\n")[39]);
     }
 }
